7559750960cec793d8b61be1cf41bf14223d5e6e
Demitros <serrasalmus.80@gmail.com>
2023-09-08 13:58:21 +0500

    added

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.netology.deliverycard {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens ru.netology.deliverycard to javafx.fxml;
-    exports ru.netology.deliverycard;
-}
--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -2,25 +2,35 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
+class DeliveryCardTest {
 
-public class DeliveryCardTest {
+    String[] monthNames = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+    String date;
+    String month;
+    String day;
+
     @BeforeEach
+    <Calendar>
     void setUp() {
         open("http://localhost:9999");
     }
 
-    // 1. Отправка заявки с верно заполненными полями;
     @Test
     public void shouldReturnSuccessIfFieldsAreFilledInCorrectly() {
 
@@ -28,16 +38,15 @@ public class DeliveryCardTest {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String dateText = deliveryDateCard.format(formatter);
 
-        $("[data-test-id=city] [placeholder='Город']").setValue("Краснодар");
+        $("[data-test-id=city] [placeholder='Город']").setValue("Челябинск");
         $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
-        $("[data-test-id=name] [type=text]").setValue("Ирина Пирогова");
-        $("[data-test-id=phone] [type=tel]").setValue("+79883339918");
+        $("[data-test-id=name] [type=text]").setValue("Алибек Мусабаев");
+        $("[data-test-id=phone] [type=tel]").setValue("+79512525252");
         $("[data-test-id=agreement]").click();
         $("[role=button] .button__content").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
         $("[data-test-id='notification'] .notification__content")
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
     }
-
 }
